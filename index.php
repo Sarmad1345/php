f349bfe60a2474fee43c3da2a08ecd4f2eedb747
Muhammad Sarmad Nadeem <sarmadnadeem3445@gmail.com>
2026-09-04 15:28:48 +0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo phpversion();
+echo "<br>";
+echo "sarmad nadeem";
+echo "<br>";
+echo "i am a student of computer science";
+echo "<br>";
+echo "i am learning php";
+ECHO 'Hello World!<br>';
+echo 'Hello World!<br>';
+
+$name = "sarmad";
+$age = 25;
+$is_student = true;
+$is_teacher = false;
+
+echo "here is my details <br>";
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Is Student: " . ($is_student ? "Yes" : "No") . "<br>";
+echo "Is Teacher: " . ($is_teacher ? "Yes" : "No") . "<br>";
+
+
+
+
+?> 
+
+
+
+
+
+
+
+   
+
+</body>
+</html>
